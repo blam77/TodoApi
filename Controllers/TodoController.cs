6a4b393439db7c3ead8883c0f5df014662ab735a
Brian Lam <brian_t_lam@homedepot.com>
2019-03-15 16:53:35 -0400

    Add POST method

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -5,35 +5,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using DiveAPI.Models;
 
-namespace DiveAPI.Controllers {
+namespace DiveAPI.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class TodoController : ControllerBase {
+    public class TodoController : ControllerBase
+    {
         private readonly TodoContext _context;
 
-        public TodoController(TodoContext context) {
+        public TodoController(TodoContext context)
+        {
             _context = context;
-            if (_context.TodoItems.Count() == 0) {
-                _context.TodoItems.Add(new TodoItem {Name = "Item1"});
-                _context.SaveChanges();
-            }
+            if (_context.TodoItems.Any()) return;
+            _context.TodoItems.Add(new TodoItem {Name = "Item1"});
+            _context.SaveChanges();
         }
 
         //GET: api/Todo
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems() {
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
             return await _context.TodoItems.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItem>> GetTodoItem(long id) {
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
             var todoItem = await _context.TodoItems.FindAsync(id);
 
-            if(todoItem == null) {
+            if (todoItem == null)
+            {
                 return NotFound();
             }
 
             return todoItem;
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new {id = item.Id}, item);
         }
     }
 }
